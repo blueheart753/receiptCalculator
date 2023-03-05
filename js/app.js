ab9f33b101b5896f1acb5ffb4769c0d0c1902b9f
kimminam <bluemind8520@gmail.com>
2023-03-05 22:13:30 +0900

    줄정리

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const accentChk = document.querySelector('button');
 accentChk.addEventListener('click', () => {
-   const price = document.querySelector('#Accent').value;
-   for(let i = 0; i<price; i++) {
-   }
+  const price = document.querySelector('#Accent').value;
+  for (let i = 0; i < price; i++) {}
 });
 // 1회차 마무리 2023 / 03 / 01
