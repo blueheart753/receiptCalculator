1167aeafa221b23dd147b4109e627faedb252ecd
kimminam <bluemind8520@gmail.com>
2023-03-05 20:15:48 +0900

    수량 추가 까지

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const accentChk = document.querySelector('button');
+accentChk.addEventListener('click', () => {
+   const price = document.querySelector('#Accent').value;
+   for(let i = 0; i<price; i++) {
+   }
+});
+// 1회차 마무리 2023 / 03 / 01
