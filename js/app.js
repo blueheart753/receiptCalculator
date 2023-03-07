d4ad7bc3753fb462d3a664325386ec96c6b559d8
kimminam <bluemind8520@gmail.com>
2023-03-07 23:44:41 +0900

    form태그로 감싸주기

--- js/app.js
+++ js/app.js
@@ -1,22 +1,11 @@
-const accentChk = document.querySelector("#submit");
-const isAccent = (value) => {
-  if (value <= 0) {
-    alert("1이상으로 입력해주세요!");
-    return;
-  }
+const accentChk = document.querySelector('#submit');
 
-  if (value >= 100) {
-    alert("100미만으로 입력해주세요!");
-    return;
-  }
-};
-accentChk.addEventListener("click", () => {
-  const price = document.querySelector("#Accent").value;
-  isAccent(price);
+accentChk.addEventListener('click', () => {
+  const price = document.querySelector('#Accent').value;
   for (let i = 1; i <= price; i++) {
     const tmp = document.createElement(`div`);
     tmp.innerHTML = `<input type = "number">`;
-    document.querySelector(".calculator").append(tmp);
+    document.querySelector('.calculator').append(tmp);
   }
 });
 // 1회차 마무리 2023 / 03 / 01
